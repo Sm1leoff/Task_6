3d5a67e4fa9a9f94e7fd5c72bf30079aeff4589b
Ivan Filippov <67921988+Sm1leoff@users.noreply.github.com>
2020-07-07 18:20:15 +0300

    Update RealizationTwo.java

--- src/com/company/RealizationTwo.java
+++ src/com/company/RealizationTwo.java
@@ -3,32 +3,32 @@ package com.company;
 import java.util.*;
 
 public class RealizationTwo {
-    public static ArrayList<Integer> kek (int [] arr) {
-        Map<Integer, ArrayList<Integer>> lol = new HashMap<>();
+    public static ArrayList<Integer> listResult (int [] arr) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
 
         int max = -1;
         int maxValue = -1;
         for (int i = 0; i <arr.length ; i++) {
-            if (lol.containsKey(arr[i])) {
+            if (map.containsKey(arr[i])) {
                 ArrayList<Integer> help = lol.get(arr[i]);
                 help.add(i);
                 if (help.size() > max) {
                     max = help.size();
                 }
-                lol.put(arr[i], help);
+                map.put(arr[i], help);
             } else {
                 ArrayList<Integer> help2 = new ArrayList<>();
                 help2.add(i);
                 if (help2.size() > max) {
                     max = help2.size();
                 }
-                lol.put(arr[i], help2);
+                map.put(arr[i], help2);
             }
         }
-        Set<Map.Entry<Integer, ArrayList<Integer>>> set = lol.entrySet();
+        Set<Map.Entry<Integer, ArrayList<Integer>>> set = map.entrySet();
         ArrayList<Integer> sol = null;
-        for (Map.Entry<Integer, ArrayList<Integer>> s: lol.entrySet()){
-            if (s.getKey() > maxValue && lol.get(s.getKey()).size()==max){
+        for (Map.Entry<Integer, ArrayList<Integer>> s: map.entrySet()){
+            if (s.getKey() > maxValue && map.get(s.getKey()).size()==max){
                 sol = s.getValue();
             }
         }
